0c7daa228f81aca4c4575306fcf9157057793d97
Mostafa <mostafa.omrane@proton.me>
2023-04-06 23:15:02 +0200

    Initial Commit

---
+++ App for apps/App_for_appsApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_for_appsApp.swift
+//  App for apps
+//
+//  Created by VegaPunk on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct App_for_appsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
